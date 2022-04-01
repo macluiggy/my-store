320d6592f76c06de49cc9e93427b1ba8fb8b5777
Luiggy Macias <ferrinluiggy@gmail.com>
2022-04-01 21:36:02 +0000

    string interpolation platizi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Nicolas';
+  age = 18;
+  img = 'https://source.unsplash.com/random';
 }
