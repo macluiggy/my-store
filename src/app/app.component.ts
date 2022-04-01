9dcfd5ca1b999c3edccb087d3399dc9606bb3300
Luiggy Macias <ferrinluiggy@gmail.com>
2022-04-01 22:17:35 +0000

    fdfdfd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ export class AppComponent {
   name = 'Nicolas';
   age = 18;
   img = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+  person = {
+    name: 'Nicolas',
+    age: 18,
+    avatar: 'https://source.unsplash.com/random',
+  };
 }
